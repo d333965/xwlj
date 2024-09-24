f6678cde39662b9a1bdb2955c62ac280cf39ebe8
d333965 <2441730566@qq.com>
2024-09-24 16:01:34 +0800

    update msg

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Navigate to the directory
+cd /home/xwlj
+
+# Fetch the latest changes from the origin repository
+git fetch origin
+
+# Merge the changes from the origin's main branch
+git merge origin/main
+
+# Stop the running Docker containers
+docker-compose down
+
+# Build the Docker containers
+docker-compose build
+
+# Start the Docker containers in detached mode
+docker-compose up -d
